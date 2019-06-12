424c9400325384a5011658354e2e1efd02ae6aa9
Jon <JonJazdzewski@gmail.com>
2019-06-11 19:06:40 -0700

    Can add characters to groups on the page where you view characters in groups

--- viewGrp.js
+++ viewGrp.js
@@ -37,7 +37,23 @@ function getCharByGroup (res,mysql,context,id){
             console.log (err);
         } else {
             context.data = rows;
-            res.render('viewGrpChar',context);
+            mysql.pool.query("SELECT Alias, ID, grpID FROM People LEFT JOIN People_Group ON ID=pepID",[id],function(err1,rows1){
+                if (err1){
+                    console.log(err1);
+                } else {
+                    var charArr =[];
+                    console.log("Grp ID= " + id);
+                    for (var cc=0;cc<rows1.length;cc++){
+                        console.log("Char: " + rows1[cc].Alias + " Group: " + rows1[cc].grpID);
+                        if (rows1[cc].grpID!=id){
+                            charArr.push(rows1[cc]);
+                            console.log("Pushing " + rows1[cc].Alias);
+                        }
+                    }
+                    context.people = charArr;
+                    res.render('viewGrpChar',context);
+                }
+            })
         }
     })
 }
@@ -85,6 +101,22 @@ router.post('/', function(req, res){
         });
     }
 });
+
+router.post('/Char/',function(req,res){
+    var mysql = req.app.get('mysql');
+    var sql = "INSERT INTO People_Groups (pepID, grpID) VALUES (?,?)";
+    var inserts = [req.body.Alias, req.body.grpID];
+    sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+        if(error){
+            console.log(JSON.stringify(error))
+            res.write(JSON.stringify(error));
+            res.end();
+        } else {
+            res.redirect('/viewGrp');
+        }
+    });
+})
+
 router.get('/:id', function (req, res) {
     callbackCount = 0;
     var context = {};
